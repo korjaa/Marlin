cd8dc477d6655da8d3c959c75c8d50087ee0df77
Jaakko Korhonen <jaakko.korh@gmail.com>
2019-10-27 19:54:52 +0200

    Remove retraction speed limit from extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -614,7 +614,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 4, 45 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 4, 1000 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
