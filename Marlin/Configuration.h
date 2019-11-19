903a3399a8ca5c2a8c01ceb6d6aad0e25cb70848
Jaakko Korhonen <jaakko.korh@gmail.com>
2019-11-19 19:14:35 +0200

    New dual drive extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -607,7 +607,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 106.66, 1280, 55 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 106.66, 1280, 80 }
 
 /**
  * Default Max Feed Rate (mm/s)
