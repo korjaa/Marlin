b97042ae64bb894acad23f4cd3c62a229b279c83
Jaakko Korhonen <jaakko.korh@gmail.com>
2021-03-07 18:19:40 +0000

    Update new TMC2209 Y-config and new TR10 z-rods

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -607,7 +607,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 106.66, 1280, 72 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 53.33, 800, 72 }
 
 /**
  * Default Max Feed Rate (mm/s)
